b3d91cabf96fa0c37a1fe30029255427335b4482
Yassin Nurmahomed <yassin791@gmail.com>
2021-04-20 17:14:24 +0200

    Move BatchRandomPooledSampler to utils

--- src/clinical_longformer/data/module.py
+++ src/clinical_longformer/data/module.py
@@ -21,44 +21,7 @@ from torchtext.experimental.functional import (
 from torchtext.experimental.transforms import basic_english_normalize
 from torchtext.vocab import Vocab
 
-
-class BatchRandomPooledSampler(Sampler):
-    """A Sampler that batches examples with text of similar sizes together in order
-    to minimize padding. Examples should be in the form of tuples (label, text).
-    """
-    def __init__(self, data_source, batch_size):
-
-        self.data_source = data_source
-
-        self.batch_size = batch_size
-
-    def __iter__(self):
-
-        indices = [(i, len(s[1])) for i, s in enumerate(self.data_source)]
-
-        random.shuffle(indices)
-
-        pooled_indices = []
-
-        p_size = self.batch_size * 100
-
-        # create pool of indices with similar lengths
-        for i in range(0, len(indices), p_size):
-
-            by_len = sorted(indices[i : i + p_size], key=lambda x: x[1])
-
-            pooled_indices.extend(by_len)
-
-        pooled_indices = [x[0] for x in pooled_indices]
-
-        batches = []
-        for i in range(0, len(pooled_indices), self.batch_size):
-            batches.append(pooled_indices[i : i + self.batch_size])
-
-        return iter(batches)
-
-    def __len__(self):
-        return len(self.data_source) // self.batch_size
+from .utils import BatchRandomPooledSampler
 
 
 class MIMICIIIDataModule(pl.LightningDataModule):
