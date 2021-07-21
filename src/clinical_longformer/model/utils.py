5980a6c8c87984f68924efb3e3dd2b11923c742d
Yassin Nurmahomed <yassin791@gmail.com>
2021-07-21 15:32:29 +0200

    Invert confusion matrix

--- src/clinical_longformer/model/utils.py
+++ src/clinical_longformer/model/utils.py
@@ -4,13 +4,14 @@ import seaborn as sns
 import torch
 
 from torchmetrics.functional import auc
+from matplotlib.axes import Axes
 
 
-def macro_auc_pr(precision, recall):
+def macro_auc_pr(precision: torch.Tensor, recall: torch.Tensor) -> torch.Tensor:
     return torch.tensor([auc(r, p) for r, p in zip(recall, precision)]).mean()
 
 
-def auc_pr(precision, recall):
+def auc_pr(precision: torch.Tensor, recall: torch.Tensor) -> torch.Tensor:
     return auc(recall, precision)
 
 
@@ -36,11 +37,25 @@ def plot_pr_curve(precision, recall):
     return fig
 
 
-def plot_confusion_matrix(confmat, index, columns):
+def plot_confusion_matrix(confmat: torch.Tensor, index: list, columns: list) -> Axes:
+    """Draw confusion matrix.
+
+    Args:
+        confmat (torch.Tensor): Confusion matrix data.
+        index (list): The labels for the x axis.
+        columns (list): The labels for the y axis.
+
+    Returns:
+        matplotlib Axes: Axes object with the confusion matrix.
+    """
 
     sns.set_theme()
 
-    df = pd.DataFrame(confmat.cpu().numpy(), index=index, columns=columns)
+    # Flip so that when binary classification TP comes first.
+    cm = confmat.flip(0, 1)
+    df = pd.DataFrame(
+        cm.cpu().numpy(), index=list(reversed(index)), columns=list(reversed(columns))
+    )
 
     plt.figure(figsize=(10, 7))
     fig = sns.heatmap(df, annot=True, cmap="Spectral").get_figure()
