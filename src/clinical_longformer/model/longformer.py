Calculate recall at fixed precision from curve

src/clinical_longformer/model/longformer.py
@@ -24,7 +24,6 @@ from transformers import (
 from torchmetrics import (
     AveragePrecision,
     AUROC,
-    BinnedRecallAtFixedPrecision,
     MetricCollection,
     PrecisionRecallCurve,
     ROC,
@@ -46,6 +45,9 @@ BERT_PRETRAINED_PATH = ".data/model/pretraining"
 # Default hyperparameters
 LEARNING_RATE = 2e-5
 BATCH_SIZE = 16
+
+
+FIXED_PRECISION = 0.8
 
 
 class BertPretrainedModule(pl.LightningModule):
@@ -101,15 +103,7 @@ class BertPretrainedModule(pl.LightningModule):
         self.valid_metrics.add_metrics([AveragePrecision(pos_label=1)])
 
         self.test_metrics = metrics.clone()
-        self.test_metrics.add_metrics(
-            [
-                AUROC(pos_label=1),
-                ROC(pos_label=1),
-                BinnedRecallAtFixedPrecision(
-                    num_classes=1, min_precision=0.8, thresholds=[0.5]
-                ),
-            ]
-        )
+        self.test_metrics.add_metrics([AUROC(pos_label=1), ROC(pos_label=1)])
 
     @staticmethod
     def add_model_specific_args(parent_parser):
@@ -304,7 +298,7 @@ class BertPretrainedModule(pl.LightningModule):
 
         self.log("AUC-ROC/test", metrics["AUROC"])
 
-        self.log("RP80/test", metrics["BinnedRecallAtFixedPrecision"][0])
+        self.log("RP80/test", recall[torch.where(precision >= FIXED_PRECISION)[0][0]])
 
         self.logger.experiment.log(
             {"PR Curve/test": wandb.Image(pr_curve), "global_step": self.global_step}

src/clinical_longformer/model/lstm.py
@@ -16,7 +16,6 @@ from torchtext.vocab import GloVe
 from torchmetrics import (
     AveragePrecision,
     AUROC,
-    BinnedRecallAtFixedPrecision,
     MetricCollection,
     PrecisionRecallCurve,
     ROC,
@@ -33,6 +32,8 @@ HIDDEN_DIM = 200
 BATCH_SIZE = 64
 EMBED_DIM = 300
 DROPOUT = 5e-1
+
+FIXED_PRECISION = 0.8
 
 
 class LSTMClassifier(pl.LightningModule):
@@ -79,15 +80,7 @@ class LSTMClassifier(pl.LightningModule):
         self.valid_metrics.add_metrics([AveragePrecision(pos_label=1)])
 
         self.test_metrics = metrics.clone()
-        self.test_metrics.add_metrics(
-            [
-                AUROC(pos_label=1),
-                ROC(pos_label=1),
-                BinnedRecallAtFixedPrecision(
-                    num_classes=1, min_precision=0.8, thresholds=[0.5]
-                ),
-            ]
-        )
+        self.test_metrics.add_metrics([AUROC(pos_label=1), ROC(pos_label=1)])
 
     @staticmethod
     def add_model_specific_args(parent_parser):
@@ -213,7 +206,7 @@ class LSTMClassifier(pl.LightningModule):
 
         self.log("AUC-ROC/test", metrics["AUROC"])
 
-        self.log("RP80/test", metrics["BinnedRecallAtFixedPrecision"][0])
+        self.log("RP80/test", recall[torch.where(precision >= FIXED_PRECISION)[0][0]])
 
         self.logger.experiment.log(
             {"PR Curve/test": wandb.Image(pr_curve), "global_step": self.global_step}