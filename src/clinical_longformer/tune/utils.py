f48244c5b900a6db7e963dedd8845e30ed79d987
Yassin Nurmahomed <yassin791@gmail.com>
2022-05-25 15:34:32 +0200

    Remove old ray tune

--- src/clinical_longformer/tune/utils.py
+++
@@ -1,9 +0,0 @@
-def trial_name_string(trial):
-    """
-    Args:
-        trial (Trial): A generated trial object.
-
-    Returns:
-        trial_name (str): String representation of Trial.
-    """
-    return trial.trial_id
