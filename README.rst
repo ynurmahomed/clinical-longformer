9f569c47fa426a672108bb3cdc3d948140b13439
Yassin Nurmahomed <yassin791@gmail.com>
2022-10-18 11:16:41 +0200

    Remove setuptools configs. Update README

--- README.rst
+++ README.rst
@@ -1,21 +1,79 @@
-===================
 clinical-longformer
 ===================
 
-
-Add a short description here!
-
+This is the source code for the Master's thesis project "Predicting hospital readmission with long clinical notes".
 
 Description
 ===========
 
-A longer description of your project goes here...
+The project aims to evaluate the effect of using a Transformer-based model with a sparse attention pattern to predict 30-day hospital readmission on a cohort from the MIMIC-III dataset.
+
+Available here is the source-code for data processing, model training and evaluation.
+
+The code was tested using Python 3.8 in a Linux environment, and it is recomended to use a [virtual environment](https://virtualenv.pypa.io/en/latest/).
+
+Setup
+=====
+
+1. Install the dependencies with:
+
+    pip3 install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cpu
+
+If you plan on using GPUS use the `reqirements-hpc.txt` files instead.
+
+Data Processing
+===============
+
+To execute data processing, run the following module:
+    
+    python -m src.clinical_longformer.data.processing
+
+To get the help messagae use the `--help` argument:
+
+```
+$ python -m src.clinical_longformer.data.processing --help
+usage: processing.py [-h] [--n-days {1-30}] [-v] [-vv] mimic_path {ds,all} {-1,512,1024,2048,4096} [out_path]
+
+Data processing
+
+positional arguments:
+mimic_path            MIMIC-III dataset path
+{ds,all}              set notes category (ds - Discharge Summary)
+{-1,512,1024,2048,4096}
+                        set note length, -1 means do not chunk text
+out_path              set output path
+
+optional arguments:
+-h, --help            show this help message and exit
+--n-days {1-30}       set number of days (only used if category is set to all)
+-v, --verbose         set loglevel to INFO
+-vv, --very-verbose   set loglevel to DEBUG
+```
+
+Training
+========
+
+Model training is done with [PyTorch Lightning](https://www.pytorchlightning.ai/) framework.
+
+There are four excutable modules available in `src/clinical_longformer/model`: `dan.py`, `lstm.py`, `bert.py` and `longformer.py`.
+These modules run the Pytorch Lightning Trainer, you can find available arguments by using the `--help` argument. 
+More information is available in the [docs](https://pytorch-lightning.readthedocs.io/en/1.5.2/common/trainer.html#trainer-flags).
+
+In the case of `longformer.py` we are able to specify the maximum token length usgin the `--max_length` argument.
+
+In the `hpc-uct`, and `chpc` folders there are examples of how to run the models.
 
 
-.. _pyscaffold-notes:
+Tuning
+======
 
-Note
-====
+Hyperparameter tuning is done using [Weights & Biases](https://docs.wandb.ai/guides/sweeps).
+Look inside `hpc-uct`, and `chpc` for examples of how to run the sweeps.
 
-This project has been set up using PyScaffold 4.0. For details and usage
-information on PyScaffold see https://pyscaffold.org/.
+
+Pre-training
+============
+
+Pre-training is done using the HuggingFace's [Transformers library](https://huggingface.co/docs/transformers/index) language-modeling example [script](https://github.com/huggingface/transformers/tree/main/examples/pytorch/language-modeling).
+
+The script has been cloned to this [repository](https://github.com/ynurmahomed/language-modeling), where job files for executing in `hpc-uct` are available.
--- src/clinical_longformer/data/processing.py
+++ src/clinical_longformer/data/processing.py
@@ -1,4 +1,7 @@
+import argparse
 import logging
+import sys
+import os
 import re
 
 import numpy as np
@@ -510,3 +513,127 @@ def build_all_notes_dataset(mimic_path, note_length, n_days, out_path):
         path = base / f"{n_days - i}days"
         path.mkdir(parents=True, exist_ok=True)
         test[i].to_csv(path / "test.csv", index=False, columns=columns)
+
+
+_logger = logging.getLogger(__name__)
+
+
+# ---- Python API ----
+
+
+def dataset(mimic_path, category, note_length, n_days, out_path):
+    """Process dataset
+
+    Args:
+      mimic_path (str): MIMIC-III dataset location
+      category (str): Clinical notes category
+      note_length (int): Note length
+      n_days (int): Number of days of notes to consider.
+
+    Returns:
+      str: MIMIC-III dataset location
+    """
+    process_notes(mimic_path, category, note_length, n_days, out_path)
+
+
+# ---- CLI ----
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--help"]``).
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(description="Data processing")
+    parser.add_argument(dest="mimic_path", help="MIMIC-III dataset path", type=str)
+    parser.add_argument(
+        dest="category",
+        help="set notes category (ds - Discharge Summary)",
+        type=str,
+        choices=["ds", "all"],
+    )
+    parser.add_argument(
+        dest="length",
+        help="set note length, -1 means do not chunk text",
+        type=int,
+        choices=[-1, 512, 1024, 2048, 4096],
+    )
+    parser.add_argument(
+        dest="out_path",
+        help="set output path",
+        type=str,
+        nargs="?",
+        default=os.getcwd(),
+    )
+    parser.add_argument(
+        "--n-days",
+        dest="n_days",
+        help="set number of days (only used if category is set to all)",
+        type=int,
+        choices=list(range(1, 31)),
+        metavar='{1-30}',
+        default=3,
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        "-vv",
+        "--very-verbose",
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action="store_const",
+        const=logging.DEBUG,
+    )
+    return parser.parse_args(args)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(
+        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+
+def main(args):
+    """Wrapper allowing :func:`fib` to be called with string arguments in a CLI fashion
+
+    Instead of returning the value from :func:`fib`, it prints the result to the
+    ``stdout`` in a nicely formated message.
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--verbose", "42"]``).
+    """
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+    _logger.info("Processing dataset...")
+    dataset(args.mimic_path, args.category, args.length, args.n_days, args.out_path)
+    _logger.info("Done processing.")
+
+
+def run():
+    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
+
+    This function can be used as entry point to create console scripts with setuptools.
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    run()
